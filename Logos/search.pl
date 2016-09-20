45d4abf5d25354ce1a0a4bf3f6b6bd7b37be74fc
Matt Currier <fingerskier@gmail.com>
2016-09-20 10:38:45 -0500

    Updated search.pl

--- Logos/search.pl
+++ Logos/search.pl
@@ -1,8 +1,22 @@
 # This script retrieves a single line (verse) from a chapter (file) in a directory (book)
 
 
-sub MAIN($book, $chapter, $verse) {
-	my @contents = "./$book/$chapter.amt".IO.lines;
+sub MAIN($ref) {
+	my @contents;
+	my @ref;
+	my @temp;
+
+	my $book;
+	my $chapter;
+	my $verse;
+
+	@ref = $ref.split(":");
+
+	$book = @ref[0];
+	$chapter = @ref[1].fmt('%02i');
+	$verse = @ref[2];
+
+	@contents = "./$book/$chapter.amt".IO.lines;
 
 	say @contents[$verse];
 }
